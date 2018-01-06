c8a3194911860fd7b9bc5a558d55d61d360bdf1e
Paulo Lima <plima@homebase.local>
2018-01-05 19:21:14 -0500

    Initial Commit

---
+++ plima-xcode-test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  plima-xcode-test
+//
+//  Created by Paulo Lima on 2018-01-05.
+//  Copyright © 2018 Paulo Lima. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
